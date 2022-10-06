1e2ed4e33c7edef323f3acf7d6a19addb26f8abd
Mary <mp@test.ca>
2022-10-06 13:42:20 -0400

    Ajout d'un nouveau projet de Test, TestProjetDeWPF et ajouter le using en haut comme reference ds la classe UnitTest.

---
+++ TestProjetDeWPF/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DeWPF;
+
+namespace TestProjetDeWPF
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
