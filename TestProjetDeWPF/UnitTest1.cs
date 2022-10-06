5f923ba96021dd4768601eae5a7744214d6e044d
Mary <mp@test.ca>
2022-10-06 14:17:41 -0400

    Ajouter les methodes test, et completer la testFace et la TestObjetHasard

--- TestProjetDeWPF/UnitTest1.cs
+++ TestProjetDeWPF/UnitTest1.cs
@@ -7,7 +7,42 @@ namespace TestProjetDeWPF
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestDe6()
+        {
+        }
+
+        [TestMethod]
+        public void TestFace()
+        {
+            int v = 2;
+            string nom = "Mary";
+
+            Assert.AreEqual(v, 2);
+            Assert.AreEqual(nom, "Mary");
+        }
+
+        [TestMethod]
+        public void TestLancer()
+        {
+        }
+
+        [TestMethod]
+        public void TestObjetHasard()
+        {
+            string nom = "MP";
+            int nbFaces = 6;
+
+            Assert.AreEqual(nom, "MP");
+            Assert.AreEqual(nbFaces, 6);
+        }
+
+        [TestMethod]
+        public void TestCreerFaces()
+        {
+        }
+
+        [TestMethod]
+        public void TestPiece()
         {
         }
     }
